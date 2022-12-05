f22594ca35cce7b799a01b67fb086e3ed9d728a1
Ryan <57397826+ryanmiura@users.noreply.github.com>
2022-12-05 09:09:46 -0300

    Create 2.c

---
+++ tecnicas_de_programacao/1revisao/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+ int valor;
+ printf("Digite um valor :\n");
+ scanf("%d",&valor);
+ if(valor%2==0){
+ printf("Esse numero e par");
+ }else{
+ printf("Esse numero e impar");
+ }
+ return 0;
+}
