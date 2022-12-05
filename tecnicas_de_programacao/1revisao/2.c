44eef179be9062f75ea0bcb9cf10bd1c43ca108d
Ryan <57397826+ryanmiura@users.noreply.github.com>
2022-12-05 09:10:43 -0300

    Update 2.c

--- tecnicas_de_programacao/1revisao/2.c
+++ tecnicas_de_programacao/1revisao/2.c
@@ -1,4 +1,7 @@
-//Faça um programa que receba um valor inteiro e apresente se o valor é par ou ímpar. Dica: use o operador % (resto da divisão de inteiros). Por exemplo: 5 % 2 retornará o resto da divisão de 5 por 2. Então, 5 % 2 = 1 (porque 5 dividido por 2 é igual a 2 e resta 1). Por outro lado, 6 % 2 = 0, porque 6 dividido por 2 é igual a 3 e resta 0.
+//Faça um programa que receba um valor inteiro e apresente se o valor é par ou ímpar. 
+//Dica: use o operador % (resto da divisão de inteiros). Por exemplo: 5 % 2 retornará o 
+//resto da divisão de 5 por 2. Então, 5 % 2 = 1 (porque 5 dividido por 2 é igual a 2 e resta 1). 
+//Por outro lado, 6 % 2 = 0, porque 6 dividido por 2 é igual a 3 e resta 0.
 
 #include <stdio.h>
 void main(){
