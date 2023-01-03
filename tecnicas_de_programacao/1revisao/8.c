559d6f419182269a672ab4162e28441908b00cef
Ryan <57397826+ryanmiura@users.noreply.github.com>
2023-01-03 17:13:01 -0300

    Create 8.c

---
+++ tecnicas_de_programacao/1revisao/8.c
@@ -0,0 +1,17 @@
+//Faça um programa que solicite 5 números positivos, some todos os números inferiores a 50 e apresente o valor da soma.
+
+#include <stdio.h>
+void main(){
+ int i;
+ float valor,result=0;
+ printf("Digite 5 numeros positivos :\n");
+ for(i=0;i<5;i++){
+ printf("Digite o %d numero: ",i+1);
+ scanf("%f",&valor);
+ if(valor<51){
+ result= result+valor;
+ }
+ }
+ printf("A soma de todos os numero menores q 50 = %.2f",result);
+ return 0;
+}
