9ea9e621c5940a8283403c630ac9a3fbd8b98ddb
Ryan <57397826+ryanmiura@users.noreply.github.com>
2023-01-03 17:13:42 -0300

    Update 8.c

--- tecnicas_de_programacao/1revisao/8.c
+++ tecnicas_de_programacao/1revisao/8.c
@@ -1,17 +1,26 @@
-//Faça um programa que solicite 5 números positivos, some todos os números inferiores a 50 e apresente o valor da soma.
+//Faça um programa que leia 3 valores inteiros e apresente: o maior valor recebido, o menor valor recebido e a média dos valores recebidos.
+
 
 #include <stdio.h>
 void main(){
- int i;
- float valor,result=0;
- printf("Digite 5 numeros positivos :\n");
- for(i=0;i<5;i++){
+ int i,valor[3],maior=0,menor,med,soma=0;
+ printf("Digite 3 numeros inteiros :\n");
+ for(i=0;i<3;i++){
  printf("Digite o %d numero: ",i+1);
- scanf("%f",&valor);
- if(valor<51){
- result= result+valor;
+ scanf("%d",&valor[i]);
+ if(i==0){
+ menor=valor[i];
  }
+ if(valor[i]>maior){
+ maior=valor[i];
+ }else if(valor[i]<menor){
+ menor=valor[i];
  }
- printf("A soma de todos os numero menores q 50 = %.2f",result);
+ soma=soma+valor[i];
+ }
+ med=soma/i;
+ printf("O maior valor e %d\n",maior);
+ printf("O menor valor e %d\n",menor);
+ printf("A media entr os valores e %d\n",med);
  return 0;
 }
